524cf5d5d23a4ef567412893329f2d8e45e10363
yahmlevi <yahmlevi@gmail.com>
2020-06-28 18:07:44 +0300

    modified work_hours.txt

--- firefox/run.sh
+++ firefox/run.sh
@@ -1,7 +1,10 @@
 
 
 # set-variable -name DISPLAY -value YOUR-IP:0.0
-HOST_IP="192.168.65.0"
+# HOST_IP="192.168.65.0"
+
+# ipconfig -> Ethernet adapter VirtualBox Host-Only Network: -> IPv4 Address
+HOST_IP="192.168.56.1"
 DISPLAY="$HOST_IP:0.0"
 
 docker run -it --rm -e DISPLAY=$DISPLAY firefox
