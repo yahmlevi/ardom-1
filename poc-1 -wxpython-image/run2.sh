892874d555c841e4a4394439fddd443b7bf2ab42
yahmlevi <yahmlevi@gmail.com>
2020-06-28 18:25:50 +0300

    modified work_hours.txt

--- poc-1 -wxpython-image/run2.sh
+++
@@ -1,9 +0,0 @@
-# Allow X server connection
-xhost +local:root
-docker run -it --rm \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    simple-gui
-# Disallow X server connection
-xhost -local:root
