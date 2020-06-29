3036048b448ea18ff23af0f83c1e27b0c77b36cf
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-06-29 15:53:32 +0300

    Added generate-ssh-keys.sh

--- 3-ssh-from-docker-to-host/docker-entrypoint.sh
+++ 3-ssh-from-docker-to-host/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
 
-cp -R /tmp/.ssh /root/.ssh
+TEMP_DIR="tmp"
+
+cp -R /$TEMP_DIR/.ssh /root/.ssh
+
 chmod 700 /root/.ssh
 chmod 644 /root/.ssh/id_rsa.pub
 chmod 600 /root/.ssh/id_rsa
