d815777cb57163260c5750f0a4cb1adda1d1019a
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-06-29 14:47:12 +0300

    Changed run.sh - mount private and public keys

--- 3-ssh-from-docker-to-host/run.sh
+++ 3-ssh-from-docker-to-host/run.sh
@@ -2,6 +2,10 @@
 set -e
 
 IMAGE_NAME="ubuntu-trusty-ssh:latest"
-docker run --rm -it --privileged --network=host -P $IMAGE_NAME sh
+# -P
+docker run --rm -it --privileged --network=host \
+    -v ~/.ssh/id_rsa:/root/.ssh/id_rsa:ro \
+    -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub:ro \
+    $IMAGE_NAME sh
 
 # ip addr show eth0
