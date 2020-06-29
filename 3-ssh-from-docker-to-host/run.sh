6eda3efae1740edcc2233753954c6eb66a1afc42
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-06-29 15:47:28 +0300

    Added generate-ssh-keys.sh

--- 3-ssh-from-docker-to-host/run.sh
+++ 3-ssh-from-docker-to-host/run.sh
@@ -3,9 +3,18 @@ set -e
 
 IMAGE_NAME="ubuntu-trusty-ssh:latest"
 # -P
-docker run --rm -it --privileged --network=host \
-    -v ~/.ssh/id_rsa:/root/.ssh/id_rsa:ro \
-    -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub:ro \
-    $IMAGE_NAME sh
+# docker run --rm -it --privileged --network=host \
+#     -v ~/.ssh/id_rsa:/root/.ssh/id_rsa:ro \
+#     -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub:ro \
+#     $IMAGE_NAME sh
 
 # ip addr show eth0
+
+
+KEY_NAME=${1:-"id_rsa"}
+
+docker run --rm -it --privileged --network=host \
+    -v /$HOME/.ssh/$KEY_NAME:/root/.ssh/id_rsa:ro \
+    -v /$HOME/.ssh/$KEY_NAME.pub:/root/.ssh/id_rsa.pub:ro \
+    $IMAGE_NAME sh
+
