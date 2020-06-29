b4158f69c363a3413b567cd80248c0134f1795be
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-06-29 15:47:28 +0300

    Added generate-ssh-keys.sh

---
+++ 3-ssh-from-docker-to-host/generate-ssh-keys.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Generating SSH keys (run in Git Bash)
+
+# https://docs.joyent.com/public-cloud/getting-started/ssh-keys/generating-an-ssh-key-manually/manually-generating-your-ssh-key-in-windows
+cd $HOME
+
+mkdir -p .ssh
+cd $HOME/.ssh
+
+KEY_NAME=${1:-"id_rsa"}
+ssh-keygen.exe -f $KEY_NAME
