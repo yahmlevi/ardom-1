9ef7d8789a26b04c3507505a39b703b668e615f5
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-07-01 12:43:57 +0300

    Changed test.dockerfile - added dos2unix

--- 3-ssh-from-docker-to-host/build.sh
+++ 3-ssh-from-docker-to-host/build.sh
@@ -5,7 +5,7 @@ set -e
 TAG=${1:-"ubuntu-trusty-ssh:latest"}
 DOCKERFILE=${2:-"test"}
 
-dos2unix docker-entrypoint.sh
-dos2unix run-ssh.sh
+# dos2unix docker-entrypoint.sh
+# dos2unix run-ssh.sh
 
 docker build -t $TAG -f $DOCKERFILE.dockerfile .
