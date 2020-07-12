8e652db701b62355a0469d2d81f8bf17c54a0853
yahmlevi <yahmlevi@gmail.com>
2020-07-12 13:29:50 +0300

    ...

--- 4-firefox-executor/run.sh
+++ 4-firefox-executor/run.sh
@@ -1,4 +1,5 @@
-
+#!/bin/bash
+set -e
 
 # set-variable -name DISPLAY -value YOUR-IP:0.0
 # HOST_IP="192.168.65.0"
@@ -9,4 +10,4 @@ HOST_IP=$(ipconfig | grep -A 4 'Ethernet adapter VirtualBox Host-Only Network:' 
 DISPLAY="$HOST_IP:0.0"
 
 IMAGE_NAME="firefox"
-docker run -it --rm -e DISPLAY=$DISPLAY $IMAGE_NAME
+docker run -it --rm -e DISPLAY="$DISPLAY" "$IMAGE_NAME"
