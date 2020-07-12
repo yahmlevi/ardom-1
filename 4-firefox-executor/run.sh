2a3aeb478c6e0f8b8a3ec482e962b6228e969918
Tsadok Levi Firseck <tlevifir@akamai.com>
2020-07-12 13:28:40 +0300

    Changed 4- .... run.sh

--- 4-firefox-executor/run.sh
+++ 4-firefox-executor/run.sh
@@ -5,8 +5,20 @@
 
 # ipconfig -> Ethernet adapter VirtualBox Host-Only Network: -> IPv4 Address
 HOST_IP=$(ipconfig | grep -A 4 'Ethernet adapter VirtualBox Host-Only Network:' | grep -A 1 'IPv4' |  cut -d ':' -f 2)
-
 DISPLAY="$HOST_IP:0.0"
 
 IMAGE_NAME="firefox"
-docker run -it --rm -e DISPLAY=$DISPLAY $IMAGE_NAME
+
+# XDG_RUNTIME_DIR
+# https://dev.to/darksmile92/run-gui-app-in-linux-docker-container-on-windows-host-4kde
+
+# XDG_RUNTIME_DIR is an environment variable that any program will access to determine the user specific directory to store small temporary files to. 
+# Normally it is set automatically when you log in.
+#
+# example
+# export XDG_RUNTIME_DIR=/home/USERNAME/tmp
+
+docker run -it --rm \
+    --env DISPLAY=$DISPLAY \
+    --env XDG_RUNTIME_DIR="/home/root/tmp" \
+    $IMAGE_NAME
