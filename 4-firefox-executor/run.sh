12d4bf05828a93a45a71bf4fb83ce9718b450133
yahmlevi <yahmlevi@gmail.com>
2020-07-12 11:46:24 +0300

    ...

--- 4-firefox-executor/run.sh
+++ 4-firefox-executor/run.sh
@@ -4,7 +4,8 @@
 # HOST_IP="192.168.65.0"
 
 # ipconfig -> Ethernet adapter VirtualBox Host-Only Network: -> IPv4 Address
-HOST_IP="192.168.56.1"
+HOST_IP=$(ipconfig | grep -A 4 'Ethernet adapter VirtualBox Host-Only Network:' | grep -A 1 'IPv4' |  cut -d ':' -f 2)
+
 DISPLAY="$HOST_IP:0.0"
 
 IMAGE_NAME="firefox"
