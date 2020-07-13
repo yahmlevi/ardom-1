ae0b06a1d6ff3b0f8a383b3a8c3caa2d4c2a27b1
yahmlevi <yahmlevi@gmail.com>
2020-07-13 14:33:18 +0300

    ...

--- 1-poc-wxpython-image/simple_gui.py
+++ 1-poc-wxpython-image/simple_gui.py
@@ -1,18 +1,18 @@
-import wx
+import wx 
+import wx.html2 
 
-class MyFrame(wx.Frame):    
-    def __init__(self):
-        super().__init__(parent=None, title='Hello World')
-        panel = wx.Panel(self)        
-        my_sizer = wx.BoxSizer(wx.VERTICAL)        
-        self.text_ctrl = wx.TextCtrl(panel)
-        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
-        my_btn = wx.Button(panel, label='Press Me')
-        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
-        panel.SetSizer(my_sizer)        
-        self.Show()
+class MyBrowser(wx.Dialog): 
+  def __init__(self, *args, **kwds): 
+    wx.Dialog.__init__(self, *args, **kwds) 
+    sizer = wx.BoxSizer(wx.VERTICAL) 
+    self.browser = wx.html2.WebView.New(self) 
+    sizer.Add(self.browser, 1, wx.EXPAND, 10) 
+    self.SetSizer(sizer) 
+    self.SetSize((700, 700)) 
 
-if __name__ == '__main__':
-    app = wx.App()
-    frame = MyFrame()
-    app.MainLoop()
+if __name__ == '__main__': 
+  app = wx.App() 
+  dialog = MyBrowser(None, -1) 
+  dialog.browser.LoadURL("http://www.google.com") 
+  dialog.Show() 
+  app.MainLoop()
