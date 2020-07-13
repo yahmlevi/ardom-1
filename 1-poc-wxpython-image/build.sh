4fac10f83295b69e61cbf43df4bd59b9e65aae22
yahmlevi <yahmlevi@gmail.com>
2020-07-13 14:05:54 +0300

    ...

--- 1-poc-wxpython-image/build.sh
+++ 1-poc-wxpython-image/build.sh
@@ -3,7 +3,14 @@ set -e
 
 TAG="simple_gui:latest"
 
-dos2unix ./run.sh
-dos2unix ./simple_gui.py
+# dos2unix ./run.sh
+# dos2unix ./simple_gui.py
 
-docker build -t $TAG -f wxpython.dockerfile .
+DOCKERFILE="wxpython.dockerfile"
+
+dos2unix $DOCKERFILE
+
+echo ""
+echo "Building docker image"
+echo "------------------------------------------"
+docker build -t $TAG -f $DOCKERFILE .
