f1be2b724623aa93abf61d6aea95ad24a12d47e1
yahmlevi <yahmlevi@gmail.com>
2020-07-14 10:58:16 +0300

    ...

--- 1-poc-wxpython-image/simple_gui.py
+++ 1-poc-wxpython-image/simple_gui.py
@@ -4,8 +4,8 @@ import wx.html2
 class MyBrowser(wx.Dialog): 
   def __init__(self, *args, **kwds): 
     wx.Dialog.__init__(self, *args, **kwds) 
-    sizer = wx.BoxSizer(wx.VERTICAL) 
-    self.browser = wx.html2.WebView.New(self) 
+    sizer = wx.BoxSizer(wx.VERTICAL)
+    self.browser = wx.html2.WebView.New(self)  
     sizer.Add(self.browser, 1, wx.EXPAND, 10) 
     self.SetSizer(sizer) 
     self.SetSize((700, 700)) 
@@ -13,17 +13,11 @@ class MyBrowser(wx.Dialog):
 if __name__ == '__main__': 
   app = wx.App() 
   dialog = MyBrowser(None, -1) 
-<<<<<<< HEAD
+  
   dialog.browser.LoadURL("http://www.google.com") 
-  dialog.Show() 
-  app.MainLoop()
-=======
-  
-  # dialog.browser.LoadURL("http://www.google.com") 
 
-  html_string = "<!doctype html><html lang='en'></html>"
+  # html_string = "<!doctype html><html lang='en'></html>"
   
-  dialog.browser.SetPage(html_string,"")
+  # dialog.browser.SetPage(html_string,"")
   dialog.Show() 
   app.MainLoop() 
->>>>>>> cf11ae5b60af7b3edbdc9c9e3212aa8e25f4670c
