4cba032a2caad87a367c8da6eda0275d08ed96a6
yahmlevi <yahmlevi@gmail.com>
2020-07-28 21:27:07 +0300

    ...

--- 5-python-virtual-env/run.bat
+++ 5-python-virtual-env/run.bat
@@ -1,7 +1,5 @@
 @ECHO OFF
 ECHO Python env with no wxPython.
 
-CMD /k "cd D:\projects & python -m venv env-test-1 & env-test-1\Scripts\activate.bat & cd D:\projects\ardom-1\5-python-virtual-env & pip install wxPython==4.1.0 & python simple_gui.py & pip list" 
+CMD /k "cd %CD% & python.exe -m venv test-1 & test-1\Scripts\activate.bat & pip install wxPython==4.1.0 & pip list & python.exe simple_gui.py"
 PAUSE
-
-## add 'run.bat' and 'simply_gui.py' to python zip file and try running from there
