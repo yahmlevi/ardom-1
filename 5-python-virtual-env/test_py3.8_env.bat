d85cd7c38ae509c44cf056eecfe7f90c1e38b6b1
yahmlevi <yahmlevi@gmail.com>
2020-08-11 19:01:45 +0300

    ...

--- 5-python-virtual-env/test_py3.8_env.bat
+++ 5-python-virtual-env/test_py3.8_env.bat
@@ -5,9 +5,9 @@ SET env_name=test_env_py3.8
 
 if exist %env_name% (
     echo "exsists"
-    CMD /k "cd d:\projects\ardom-1\5-python-virtual-env & %env_name%\Scripts\activate.bat & python3.8 simple_gui.py"
+    CMD /k "cd d:\projects\ardom-1\5-python-virtual-env & %env_name%\Scripts\activate.bat & pip install wxpython & python3.8 simple_gui.py exit"
 ) else (
     echo "dont exsists"
-    CMD /k "cd d:\projects\ardom-1\5-python-virtual-env & python -m venv %env_name% & %env_name%\Scripts\activate.bat & python3.8 simple_gui.py"
+    CMD /k "cd d:\projects\ardom-1\5-python-virtual-env & C:\Users\Yahm\AppData\Local\Programs\Python\Python38-32\python.exe -m venv %env_name% & %env_name%\Scripts\activate.bat & pip install wxpython & python3.8 simple_gui.py"
 )
 PAUSE
