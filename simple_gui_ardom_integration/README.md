656f9244b46ca899531008ced5aa8c49435d7cc6
yahmlevi <yahmlevi@gmail.com>
2020-08-12 12:32:26 +0300

    ...

---
+++ simple_gui_ardom_integration/README.md
@@ -0,0 +1,8 @@
+steps for running an application:
+
+1 - set up python as instructed in the PowerPoint presentation (Set up new Python interpeter.pptx)
+
+2 - modifey .bat file as needed
+
+3 - double click on launch_bat.vbs to execute .bat file without shoing cmd
+source: https://www.winhelponline.com/blog/run-bat-files-invisibly-without-displaying-command-prompt/
--- simple_gui_ardom_integration/wx_simplegui_example_tamplate.bat
+++ simple_gui_ardom_integration/wx_simplegui_example_tamplate.bat
@@ -3,7 +3,7 @@ ECHO Python3.8 env with wxPython simple_gui.py
 ECHO Template is compatible with all python3 and above versions 
 
 SET env_name=ardom_wx_test_env
-SET py_ver=python3.8
+SET py_ver=pythonw
 SET script_name=simple_gui.py
 
 if exist %env_name% (
@@ -13,4 +13,4 @@ if exist %env_name% (
     echo "env doesn't exist"
     CMD /k "cd %cd% & python -m venv %env_name% & %env_name%\Scripts\activate.bat & pip install -r requirements.txt & %py_ver% %script_name%"
 )
-PAUSE
+EXIT
