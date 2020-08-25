2666a3ed5a40115330922079e1b03bf6863cf3b5
yahmlevi <yahmlevi@gmail.com>
2020-08-25 18:26:04 +0300

    ...

--- file_crawler_project/file_crawler.py
+++ file_crawler_project/file_crawler.py
@@ -5,11 +5,6 @@ from datetime import datetime
 from hurry.filesize import size
 
 
-# get path from user and cd to it
-path = input("Enter Desired Path:")
-os.chdir(path)
-
-#path = "D:\projects\ardom-1"
 
 class File:
     pass
@@ -22,10 +17,8 @@ def get_modified_year(file_name):
   modified_year = modified_date.split("-")
   return modified_year[0]
 
-# TODO - complete function
 def get_file_size_in_mg(file_name):
     return size(os.stat(file_name).st_size)
-    # return (os.stat(file_name).st_size)
 
 def get_file_size(file_name):
     return os.stat(file_name).st_size
@@ -39,13 +32,13 @@ def get_file_type(file_name):
 
 
 
-def process_list(file_list, year_list, file_type_list):
+def get_totals_by_year_and_file_type(file_list, year_list, file_type_list):
     totals_list = []
    
     for year in year_list:
         for file_type in file_type_list:
 
-            count, total_size = get_info_by_year_and_file_type(file_list, lambda file: file.year == year and file.file_type == file_type)
+            count, total_size = get_info_by_filter(file_list, lambda file: file.year == year and file.file_type == file_type)
 
             if count > 0:
                 totals = Totals()
@@ -58,6 +51,21 @@ def process_list(file_list, year_list, file_type_list):
     return totals_list
 
 
+def get_totals(file_list):
+    totals_list = []
+   
+    # the filter is always true (we take all files)
+    count, total_size = get_info_by_filter(file_list, lambda file: True)
+
+    if count > 0:
+        totals = Totals()
+        totals.total_size = total_size
+        totals.total_size_str = size(total_size)
+        totals.count = count
+        totals_list.append(totals)
+    return totals_list
+
+
 def count_by_year_and_file_type(file_list, filter):
     result = 0
     for file in file_list:
@@ -66,7 +74,7 @@ def count_by_year_and_file_type(file_list, filter):
     return result
 
 
-def get_info_by_year_and_file_type(file_list, filter):
+def get_info_by_filter(file_list, filter):
     count = 0
     size = 0
     for file in file_list:
@@ -101,42 +109,39 @@ def filter_by_nesting_level_2(file_list, nesting_level=2):
     return result
 
 
-def populate_list():
+def populate_list(path):
     file_list = []
     year_list = []
     file_type_list = []
     restricted_files_list = []
 
     for root, dirs, files in os.walk(".", topdown=False):
-        #print ("root: " + root)
-
         # for dir_name in dirs:
         #     #  for root, dirs, files in os.walk(".", topdown=False):
         #     print(dir_name)    
 
-
         for file_name in files:
+            try:   
+                file_path = os.path.join(root, file_name)
+                file_type =  get_file_type(file_path)
+                
+                if file_type != "FOLDER":
+                    file = File()
 
-            file = File()
+                    file.root = root
+                    file.path = file_path 
+                    file.file_type = file_type
+                    file.year = get_modified_year(file.path) 
+                    file.file_size = get_file_size(file.path)
 
-            try:
-                    
-                file.root = root
-                file.path = os.path.join(root, file_name)
-                file.year = get_modified_year(file.path)
-                file.file_type = get_file_type(file.path)
-                # file.file_size = get_file_size_in_mg(file.path)
-                file.file_size = get_file_size(file.path)
+                    file_list.append(file)
 
-                file_list.append(file)
+                    if file.year not in year_list:
+                        year_list.append(file.year)
 
-                #print("---" + file.path)
+                    if file.file_type not in file_type_list:
+                        file_type_list.append(file.file_type)
 
-                if file.year not in year_list:
-                    year_list.append(file.year)
-
-                if file.file_type not in file_type_list:
-                    file_type_list.append(file.file_type)
             except OSError:
                 restricted_files_list.append(os.path.abspath(file_name))
                 print("error") 
@@ -145,49 +150,69 @@ def populate_list():
 
 def print_files(file_list):
     for file in file_list:
-        # print(file.year + ", " + file.path)
         print(file.root + ", " + file.path)
 
-
-
-
-file_list, year_list, file_type_list, restricted_files_list = populate_list()
-
-totals_list = process_list(file_list, year_list, file_type_list)
-
-#totals_list_filtered = process_list(filter_by_nesting_level(file_list), year_list, file_type_list)
 
 def print_totals(totals_list):
     for totals in totals_list:
         print (totals.year + " " + totals.file_type + " count: " + str(totals.count) + " size in bytes: " + str(totals.total_size) + " size: " + totals.total_size_str)
-#    print (totals.year)
-#    print(totals.file_type)
-#    print(totals.count)
-
-excel = Excel()
-
-excel.insert_to_root(totals_list, path)
-#insert_to_excel.insert_to_excel_subroot(totals_list_filtered, path)
-
-excel.insert_to_restricted(restricted_files_list)
-
-#print_files(file_list)
-
-# for totals in totals_list_filtered:
-#         print(totals.year + ", " + str(totals.total_size))
-
-d = filter_by_nesting_level_2(file_list)
-
-for sub_path in d.keys():
-    file_list = d[sub_path]
-    totals_list = process_list(file_list, year_list, file_type_list)
-    # print_totals(totals_list)
-    print("")
-    print (sub_path)
-    print ("------------------------------------------------------")
-    print_totals(totals_list) 
-    excel.insert_to_subroot(totals_list, sub_path)
 
 
-excel.close()
+def main():
+    
+    # get path from user and cd to it
+    path = input("Enter Desired Path:")
+    os.chdir(path)
 
+    excel = Excel()
+
+    file_list, year_list, file_type_list, restricted_files_list = populate_list(path)
+
+    #
+    # 1st tab - Root
+    #
+    totals_list = get_totals_by_year_and_file_type(file_list, year_list, file_type_list)
+    excel.insert_to_root(totals_list, path)
+    
+    #
+    # 2nd tab - Sub Root
+    #
+    d = filter_by_nesting_level_2(file_list)
+
+    for sub_path in d.keys():
+        file_list = d[sub_path]
+        totals_list = get_totals_by_year_and_file_type(file_list, year_list, file_type_list)
+
+        # print("")
+        # print (sub_path)
+        # print ("------------------------------------------------------")
+        print_totals(totals_list) 
+        excel.insert_to_subroot(totals_list, sub_path)
+
+    #
+    # 3rd tab - totals 
+    # 
+    total_tab_totals_list = get_totals(file_list)
+    excel.insert_to_total(total_tab_totals_list, path, "")
+
+    for sub_path in d.keys():
+        file_list = d[sub_path]
+        totals_list = get_totals(file_list)
+        
+        # print("")
+        # print (sub_path)
+        # print ("------------------------------------------------------")
+        # # print_totals(totals_list) 
+        excel.insert_to_total(totals_list, path, sub_path)
+
+    #
+    # 4th tab - Log Restricted
+    #
+    excel.insert_to_restricted(restricted_files_list)
+
+    excel.close()
+
+
+
+# call main
+main()
--- file_crawler_project/insert_to_excel.py
+++ file_crawler_project/insert_to_excel.py
@@ -13,105 +13,130 @@ class Excel(object):
 
         # Add a bold format to use to highlight cells.
         self.format_bold = self.workbook.add_format({'bold': True})
+
+        self.worksheet_data = [
+            {
+                "name": "Root",
+                "headers": ["Root Folder", "Year Modified", "File Type", "Size in bytes",  "Size", "count"]
+            },
+            {
+                "name": "Sub-Root",
+                "headers": ["Subroot Folder", "Year Modified", "File Type", "Size in bytes", "Size", "Count"]
+            },
+            {
+                "name": "Total", 
+                "headers": ["Root Folder", "Subroot Folder", "File Size", "Count"]
+            },
+            {
+                "name": "Log-Restricted",
+                "headers": ["Restricted File Path"]
+            }
+        ]
         
         self.init_worksheets()
 
     def init_worksheets(self):
-        bold = self.format_bold
 
-        self.worksheets["root"] = self.workbook.add_worksheet("root")
+        for data in self.worksheet_data:
+            name = data["name"]
+            self.worksheets[name] = self.workbook.add_worksheet(name)
+            worksheet = self.worksheets[name]
+            
+            self.set_column_headers(worksheet, data["headers"])
+            self.new_row_indices[name] = 1        
+
+    def set_column_headers(self, worksheet, headers):
         
-        worksheet = self.worksheets["root"]
-        
-        # create headlines
-        worksheet.write("A1", "Root Folder", bold)
-        worksheet.write("C1", "Year Modified", bold)
-        worksheet.write("E1", "File Type", bold)
-        worksheet.write("G1", "Size in bytes", bold)
-        worksheet.write("I1", "Size", bold)
-        worksheet.write("K1", "count", bold)
+        column_index = 0
+        for header in headers:
+            worksheet.write(0, column_index, header, self.format_bold)
+            if column_index == 0:
+                column_width = 40
+            else:
+                column_width = 20
+            worksheet.set_column(column_index, column_index, column_width)
+            column_index+=1 
 
-        self.new_row_indices["root"] = 1
-        
-        self.worksheets["subfolders"] = self.workbook.add_worksheet("subroot")
-
-        worksheet = self.worksheets["subfolders"]
-    
-        # create headlines
-        worksheet.write("A1", "Subroot Folder", bold)
-        worksheet.write("C1", "Year Modified", bold)
-        worksheet.write("E1", "File Type", bold)
-        worksheet.write("G1", "Size in bytes", bold)
-        worksheet.write("I1", "Size", bold)
-        worksheet.write("K1", "count", bold)
-
-        self.new_row_indices["subfolders"] = 1
-        
-        self.worksheets["restricted"] = self.workbook.add_worksheet("log-restricted")
-
-        worksheet = self.worksheets["restricted"]
-    
-        # create headlines
-        worksheet.write("A1", "restricted file path", bold)
-
-        self.new_row_indices["restricted"] = 1
-        
 
     def insert_to_root(self, totals_list, path):
 
-        worksheet = self.worksheets["root"]
+        name = self.worksheet_data[0]["name"]
+        worksheet = self.worksheets[name]
 
         # Start from the first cell. Rows and columns are zero indexed.
-        row =  self.new_row_indices["root"] 
+        row =  self.new_row_indices[name] 
 
         # write date
         for totals in totals_list:
             if totals.file_type != "FOLDER":
                 worksheet.write(row, 0, path)
-                worksheet.write(row, 2, totals.year)
-                worksheet.write(row, 4, totals.file_type)
-                worksheet.write(row, 6, totals.total_size)
-                worksheet.write(row, 8, totals.total_size_str)
-                worksheet.write(row, 10, totals.count)        
+                worksheet.write(row, 1, totals.year)
+                worksheet.write(row, 2, totals.file_type)
+                worksheet.write(row, 3, totals.total_size)
+                worksheet.write(row, 4, totals.total_size_str)
+                worksheet.write(row, 5, totals.count)        
                 row += 1
 
-        self.new_row_indices["root"] = row
+        self.new_row_indices[name] = row
 
 
     def insert_to_subroot(self, totals_list, path):
 
-        worksheet = self.worksheets["subfolders"]
+        name = self.worksheet_data[1]["name"]
+        worksheet = self.worksheets[name]
     
         # Start from the first cell. Rows and columns are zero indexed.
-        row =  self.new_row_indices["subfolders"]
+        row =  self.new_row_indices[name]
 
         # write date
         for totals in totals_list:
             if totals.file_type != "FOLDER":
                 worksheet.write(row, 0, path)
-                worksheet.write(row, 2, totals.year)
-                worksheet.write(row, 4, totals.file_type)
-                worksheet.write(row, 6, totals.total_size)
-                worksheet.write(row, 8, totals.total_size_str)
-                worksheet.write(row, 10, totals.count)        
+                worksheet.write(row, 1, totals.year)
+                worksheet.write(row, 2, totals.file_type)
+                worksheet.write(row, 3, totals.total_size)
+                worksheet.write(row, 4, totals.total_size_str)
+                worksheet.write(row, 5, totals.count)        
                 row += 1
 
-        self.new_row_indices["subfolders"] = row
+        self.new_row_indices[name] = row
+
+
+    def insert_to_total(self, totals_list, path, sub_path = ""):
+
+        name = self.worksheet_data[2]["name"]
+        worksheet = self.worksheets[name]
+    
+        # Start from the first cell. Rows and columns are zero indexed.
+        row =  self.new_row_indices[name]
+
+        # write date
+        for totals in totals_list:
+        
+            worksheet.write(row, 0, path)
+            worksheet.write(row, 1, sub_path)
+            worksheet.write(row, 2, totals.total_size_str)
+            worksheet.write(row, 3, totals.count)    
+            row += 1
+
+        self.new_row_indices[name] = row
+
 
     
     def insert_to_restricted(self, restricted_files_list):
 
-        worksheet = self.worksheets["restricted"]
+        name = self.worksheet_data[3]["name"]
+        worksheet = self.worksheets[name]
 
         # Start from the first cell. Rows and columns are zero indexed.
-        row =  self.new_row_indices["restricted"] 
+        row =  self.new_row_indices[name] 
 
         
         for item in restricted_files_list:  
             worksheet.write(row, 0, item)
             row += 1
 
-        self.new_row_indices["root"] = row
+        self.new_row_indices[name] = row
 
     
     
