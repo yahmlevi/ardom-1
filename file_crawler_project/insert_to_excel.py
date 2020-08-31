5392a6223418d6fe89c20bbaeea41042dc35fdec
yahmlevi <yahmlevi@gmail.com>
2020-08-31 15:36:48 +0300

    ...

--- file_crawler_project/insert_to_excel.py
+++ file_crawler_project/insert_to_excel.py
@@ -1,13 +1,19 @@
 import xlsxwriter
 import os
+from datetime import date
+
 
 
 class Excel(object):
 
-    def __init__(self):
-        
+    def __init__(self, file_path):
+
         # create a workbook and add a worksheet.
-        self.workbook = xlsxwriter.Workbook('D:\projects\\ardom-1\\file_crawler_project\\file_crawler_results.xlsx')
+        self.today_date = date.today()
+        self.file_name = 'file_crawler_results_{}.xlsx'.format(self.today_date)
+        self.file = os.path.join(file_path, self.file_name)
+
+        self.workbook = xlsxwriter.Workbook(self.file)
         self.worksheets = {}
         self.new_row_indices = {}
 
@@ -17,11 +23,11 @@ class Excel(object):
         self.worksheet_data = [
             {
                 "name": "Root",
-                "headers": ["Root Folder", "Year Modified", "File Type", "Size in bytes",  "Size", "count"]
+                "headers": ["Root Folder", "Year Modified", "File Type", "Size in GB",  "Size", "Count"]
             },
             {
                 "name": "Sub-Root",
-                "headers": ["Subroot Folder", "Year Modified", "File Type", "Size in bytes", "Size", "Count"]
+                "headers": ["Root Folder", "Subroot Folder", "Year Modified", "File Type", "Size in GB", "Size", "Count"]
             },
             {
                 "name": "Total", 
@@ -30,6 +36,10 @@ class Excel(object):
             {
                 "name": "Log-Restricted",
                 "headers": ["Restricted File Path"]
+            },
+            {
+                "name": "Root by File Type",
+                "headers": ["Root Folder", "File Type", "Size in GB", "Size", "Count"]
             }
         ]
         
@@ -50,7 +60,7 @@ class Excel(object):
         column_index = 0
         for header in headers:
             worksheet.write(0, column_index, header, self.format_bold)
-            if column_index == 0:
+            if column_index == 0 or 1:
                 column_width = 40
             else:
                 column_width = 20
@@ -80,7 +90,7 @@ class Excel(object):
         self.new_row_indices[name] = row
 
 
-    def insert_to_subroot(self, totals_list, path):
+    def insert_to_subroot(self, totals_list, sub_path, path):
 
         name = self.worksheet_data[1]["name"]
         worksheet = self.worksheets[name]
@@ -92,11 +102,12 @@ class Excel(object):
         for totals in totals_list:
             if totals.file_type != "FOLDER":
                 worksheet.write(row, 0, path)
-                worksheet.write(row, 1, totals.year)
-                worksheet.write(row, 2, totals.file_type)
-                worksheet.write(row, 3, totals.total_size)
-                worksheet.write(row, 4, totals.total_size_str)
-                worksheet.write(row, 5, totals.count)        
+                worksheet.write(row, 1, sub_path)
+                worksheet.write(row, 2, totals.year)
+                worksheet.write(row, 3, totals.file_type)
+                worksheet.write(row, 4, totals.total_size)
+                worksheet.write(row, 5, totals.total_size_str)
+                worksheet.write(row, 6, totals.count)        
                 row += 1
 
         self.new_row_indices[name] = row
@@ -137,6 +148,27 @@ class Excel(object):
             row += 1
 
         self.new_row_indices[name] = row
+
+    def insert_to_root_by_file_type(self, totals_list, path):
+        
+        name = self.worksheet_data[4]["name"]
+        worksheet = self.worksheets[name]
+
+        # Start from the first cell. Rows and columns are zero indexed.
+        row =  self.new_row_indices[name] 
+
+        # write date
+        for totals in totals_list:
+            if totals.file_type != "FOLDER":
+                worksheet.write(row, 0, path)
+                worksheet.write(row, 1, totals.file_type)
+                worksheet.write(row, 2, totals.total_size)
+                worksheet.write(row, 3, totals.total_size_str)
+                worksheet.write(row, 4, totals.count)        
+                row += 1
+
+        self.new_row_indices[name] = row
+
 
     
     
