31ae7bd12be7e316e708bfb5929587e4ffcb7404
yahmlevi <yahmlevi@gmail.com>
2020-09-17 14:59:52 +0300

    ...

---
+++ file_crawler_project/root_run.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+ECHO Root Analysis
+
+SET env_name=crawler_env
+SET py_ver=python
+SET script_name=file_crawler.py
+SET analysis_type="root"
+
+if exist %env_name% (
+    echo "env exists"
+    CMD /k "cd %cd% & %env_name%\Scripts\activate.bat & pip install -r requirements.txt & %py_ver% %script_name% %analysis_type%"
+) else (
+    echo "env doesn't exist"
+    CMD /k "cd %cd% & python -m venv %env_name% & %env_name%\Scripts\activate.bat & pip install -r requirements.txt & %py_ver% %script_name% %analysis_type%"
+)
+PAUSE
