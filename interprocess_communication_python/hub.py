1f5f2733033bc437498da7694204557c7a59823a
yahmlevi <yahmlevi@gmail.com>
2020-10-18 12:01:55 +0300

    ...

--- interprocess_communication_python/hub.py
+++ interprocess_communication_python/hub.py
@@ -1,51 +1,15 @@
-import time
+import persistqueue
 import threading
-import queue
-#import json
+import time
 
-# B SIDE
 class Hub(threading.Thread):
     
-    def __init__(self, queues: dict):
+    def __init__(self, path):
         threading.Thread.__init__(self)
-        self.queues = queues
-
-        self.running = True
-
-
-
-    def run(self):
-        while self.running:
-            for q_item in self.queues.values():
-                
-                q_out: queue.Queue = q_item["q_out"]
-                q_in: queue.Queue  = q_item["q_in"]
-
-                u_id: str = q_item["uid"]
-
-                if not q_out.empty():
-                    value_from_q = q_out.get()
-                    print("B SIDE: Received value '{}' from thread '{}'".format(value_from_q, u_id) )
-
-                data = self.get_data_by_uid(u_id)
-                q_in.put("B SIDE: data")   
-
-                #else:
-                #    print("queue '{}' is empty".format(u_id))
-            time.sleep(1)
-
-    def stop(self):
-        self.running = False
-
-
-    def get_data_by_uid(self, u_id):
-        x = {"uid":"one", "data":30,
-            "uid":"two", "data":40,
-            "uid":"three", "data":50,
-            "uid":"four", "data":80}
-        print(u_id)
+        self.q =  persistqueue.FIFOSQLiteQueue(path=path, multithreading=True, auto_commit=True, db_file_name="my_file_name1", timeout=0.5)  
         
-        #print(x.get[u_id])
-
-        return "Data for {}".format(u_id)
-
+    def run(self):
+        while True:  
+            if not self.q.empty():
+                print(self.q.get())
+                time.sleep(0.5)
--- interprocess_communication_python/test.py
+++ interprocess_communication_python/test.py
@@ -1,14 +1,37 @@
-#import persistqueue
+import persistqueue
 import threading
+import time
+import sys
 
 class Hub(threading.Thread):
     
-    def __init__(self, path, queue):
+    def __init__(self, path, stream_uid_list):
         threading.Thread.__init__(self)
-        #self.q = persistqueue.SQLiteQueue(path, auto_commit=True)
-        self.q = queue
+
+        self.telnet_stream_q_in_list = []
+        for u_id in stream_uid_list:
+            q_in =  persistqueue.FIFOSQLiteQueue(path=path, multithreading=True, auto_commit=True, db_file_name=u_id, timeout=0.5)  
+            self.telnet_stream_q_in_list.append(q_in)
+
+        self.q = persistqueue.FIFOSQLiteQueue(path=path, multithreading=True, auto_commit=True, db_file_name="my_file_name", timeout=10.0)
+        print("Hub is ready")
 
     def run(self):
-        if not self.q.empty():
-            print(self.q.get())
+        while True:
+
+            try:    
+                if not self.q.empty():
+                    print(self.q.get())
+                
+                for q_in in self.telnet_stream_q_in_list:
+                    # print("Putting value in queue ")
+                    q_in.put("Some value")
+
+            except:
+                ex = sys.exc_info()[0]
+                print ("Exception in Hub - {}".format(ex))
+                time.sleep(1.2)
+                
+            time.sleep(0.5)
+
 
