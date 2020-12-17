741c89353a41e3817aababdfcbfa916b7cf56faf
yahmlevi <yahmlevi@gmail.com>
2020-12-17 22:34:22 +0200

    ...

--- servers_query_automation/tkinter_gui_test.py
+++ servers_query_automation/tkinter_gui_test.py
@@ -8,32 +8,38 @@ class GUI():
 
         master.title("Query Automation App")
 
+        self.bottomframe = Frame(master)
+        self.bottomframe.pack(side=BOTTOM)
+        
+        self.topframe = Frame(master)
+        self.topframe.pack(side=TOP)
+
+
         self.create_left_panel(master, functions)
         self.create_right_panel(master, functions)
-        
 
     def create_left_panel(self, master, functions):
 
         hostname_to_os_version_dict = functions.extract_hostname_os_version_dict()
 
-        excel_btn = Button(master, text="Excel")
-        excel_btn.grid(row=0, column=1,columnspan=3)
-        ad_btn = Button(master, text="ActiveDirectory")
-        ad_btn.grid(row=0, column=2)
-        both_btn = Button(master, text="Both")
-        both_btn.grid(row=0, column=3)
+        left_excel_btn = Button(self.topframe, text="Excel")
+        left_excel_btn.pack(side=LEFT)
+        left_ad_btn = Button(self.topframe, text="ActiveDirectory")
+        left_ad_btn.pack(side=LEFT)
+        left_both_btn = Button(self.topframe, text="Both")
+        left_both_btn.pack(side=LEFT)
 
-        select_all_chk = Checkbutton(master,text="select all")
-        select_all_chk.grid(row=0, column=0)
+        left_select_all_chk = Checkbutton(self.topframe,text="select all")
+        left_select_all_chk.pack(side=LEFT)
 
         scrollbar = Scrollbar(master, orient=VERTICAL) # orient=VERTICAL
-        scrollbar.grid(row=1,column=0, sticky="W")
+        scrollbar.pack(side=LEFT)
 
         hostname_list = Listbox(root,height=29, width=40) # , yscrollcommand = scrollbar.set
         for hostname in hostname_to_os_version_dict:
             hostname_list.insert(END, hostname)
 
-        hostname_list.grid(row=1,column=1,columnspan=3,sticky='W') #,sticky='nsew'
+        hostname_list.pack(side=LEFT)
         scrollbar.config(command=hostname_list.yview)
 
 
@@ -41,24 +47,24 @@ class GUI():
 
         hostname_to_os_version_dict = functions.extract_hostname_os_version_dict()
 
-        btn = Button(master, text="Excel")
-        btn.grid(row=0, column=4)
-        btn = Button(master, text="ActiveDirectory")
-        btn.grid(row=0, column=5)
-        btn = Button(master, text="Both")
-        btn.grid(row=0, column=6)
+        right_btn = Button(master, text="Excel")
+        right_btn.pack(side=RIGHT)
+        right_btn = Button(master, text="ActiveDirectory")
+        right_btn.pack(side=RIGHT)
+        right_btn = Button(master, text="Both")
+        right_btn.pack(side=RIGHT)
 
-        chk = Checkbutton(master,text="select all")
-        chk.grid(row=0, column=6)
+        right_select_all_chk = Checkbutton(master,text="select all")
+        right_select_all_chk.pack(side=RIGHT)
 
         scrollbar = Scrollbar(master) # orient=VERTICAL
-        scrollbar.grid(row=1,column=7,rowspan=3)
+        scrollbar.pack(side=RIGHT)
 
         hostname_list = Listbox(root,height=29, width=40, bg="red") # , yscrollcommand = scrollbar.set
         for hostname in hostname_to_os_version_dict:
             hostname_list.insert(END, hostname)
 
-        hostname_list.grid(row=1,column=7,columnspan=3,sticky='E') #,sticky='nsew'
+        hostname_list.pack(side=RIGHT)
         scrollbar.config(command=hostname_list.yview)
 
 
@@ -66,9 +72,6 @@ class GUI():
 
 root = Tk()
 root.geometry("750x500")
-for i in range(0,50):
-    root.grid_rowconfigure(i, weight=1) # this needed to be added
-    root.grid_columnconfigure(i, weight=1)
 window = GUI(root)
 root.mainloop()
 
